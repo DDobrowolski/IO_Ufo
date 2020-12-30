4fd52f164f34dfe892d53f468c91145241f1e27f
DDobrowolski <damian.dobrowolski99@gmail.com>
2020-12-30 20:54:54 +0100

    Added Active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new'
