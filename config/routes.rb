6893093a1a0fbb1b53e684e6724b85769f97fcde
DDobrowolski <damian.dobrowolski99@gmail.com>
2020-12-30 18:44:34 +0100

    Added auth logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
+    get 'sign_up', to: 'users/registrations#new'
+    get 'forgot_password', to: 'users/passwords#new'
+    get 'reset_password', to: 'users/passwords#edit'
+  end
+  root to: 'application#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
